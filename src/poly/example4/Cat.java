5ea840989d0c71a7e1f58217a82868a28344b75b
Headh1 <a.design804@gmail.com>
2023-12-17 23:01:25 +0900

    다형성

---
+++ src/poly/example4/Cat.java
@@ -0,0 +1,14 @@
+package poly.example4;
+
+public class Cat extends AbstractAnimal {
+
+    @Override
+    public void sound() {
+        System.out.println("야옹");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("살금살금");
+    }
+}
