90f4a482821634c5727e9c21700ed1ae136a0a3f
Headh1 <a.design804@gmail.com>
2023-12-17 17:54:11 +0900

    super

---
+++ src/extends1/super2/Super2Main.java
@@ -0,0 +1,9 @@
+package extends1.super2;
+
+public class Super2Main {
+    public static void main(String[] args) {
+        System.out.println("start");
+        ClassC classC = new ClassC();
+        System.out.println("end");
+    }
+}
